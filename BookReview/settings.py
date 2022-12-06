612d2f1e6e14106d6787255530c57a3159dffab0
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2022-12-06 19:35:07 +0530

    Changing

--- BookReview/settings.py
+++ BookReview/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0yb$byd7=-q05%d!x-lulbg3s#&b*5$l6pi3#e7itj-(xbi-r!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
